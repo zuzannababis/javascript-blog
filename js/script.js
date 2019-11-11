7824edaded22257d76d5807ab28681fb610593af
Zuza Babis <zuzanna.babis@gmail.com>
2019-11-11 17:15:01 +0100

    Add class active

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
+'use strict';
+
 /* document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   }); */
 
 const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event);
-}
+const clickedElement = this;
+console.log('Link was clicked!');
+console.log(event);
 
-    /* remove class 'active' from all article links  */
+
+    /* [DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
 }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+console.log('clickedElement (with plus): ', clickedElement);
+clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.posts article.active');
 
 for(let activeArticle of activeArticles){
@@ -28,7 +34,7 @@ for(let activeArticle of activeArticles){
     /* find the correct article using the selector (value of 'href' attribute) */
 
     /* add class 'active' to the correct article */
-
+}
 
 const links = document.querySelectorAll('.titles a');
   
