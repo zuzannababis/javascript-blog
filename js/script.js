f64a0dee7a2f6adda3366e3b8c2a99f42b5f2339
Zuza Babis <zuzanna.babis@gmail.com>
2019-11-11 19:57:16 +0100

    Amend code

--- js/script.js
+++ js/script.js
@@ -6,51 +6,52 @@
   }); */
 
 const titleClickHandler = function(event){
-event.preventDefault();
-const clickedElement = this;
+    event.preventDefault();
+    const clickedElement = this;
 
-console.log('Link was clicked!');
-console.log(event);
+    console.log('Link was clicked!');
+    console.log(event);
 
 
     /* [DONE] remove class 'active' from all article links  */
 
-const activeLinks = document.querySelectorAll('.titles a.active');
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-}
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
     /* [DONE] add class 'active' to the clicked link */
 
-console.log('clickedElement: ', clickedElement);
+    console.log('clickedElement: ', clickedElement);
 
-clickedElement.classList.add('active');
+    clickedElement.classList.add('active');
 
     /* [DONE] remove class 'active' from all articles */
 
-const activeArticles = document.querySelectorAll('.posts article.active');
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
-for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-}
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+
     /* get 'href' attribute from the clicked link */
 
-const articleSelector = clickedElement.getAttribute('href');
+    const articleSelector = clickedElement.getAttribute('href');
 
-console.log('articleSelector', articleSelector);
+    console.log('articleSelector', articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
 
-const targetArticle = document.querySelector(articleSelector);
+    const targetArticle = document.querySelector(articleSelector);
 
-console.log('targetArticle', targetArticle);
+    console.log('targetArticle', targetArticle);
 
     /* add class 'active' to the correct article */
 
-targetArticle.classList.add('active');
+    targetArticle.classList.add('active');
 
-console.log('targetArticle', 'active');
+    console.log('targetArticle', 'active');
 
 }
 
