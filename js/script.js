d1fde28fe10fb034a2a056d7f57593b4fe18821a
Zuza Babis <zuzanna.babis@gmail.com>
2019-11-17 17:34:57 +0100

    Add tags

--- js/script.js
+++ js/script.js
@@ -112,27 +112,54 @@ generateTitleLinks();
 function generateTags (){
     /* find all articles */
 
+    const articles = document.querySelectorAll(optArticleSelector);
+
     /* START LOOP: for every article: */
+
+    for(let article of articles){
 
         /* find tags wrapper */
 
+        const tagsWrapper = article.querySelector(optArticleTagsSelector);
+
         /* make html variable with empty string */
+
+        let html = '';
 
         /* get tags from data-tags attribute */
 
+        const articleTags = article.getAttribute('data-tags');
+
+        console.log('articleTags', articleTags);
+
         /* split tags into array */
+
+        const articleTagsArray = articleTags.split(' ');
+        console.log(articleTagsArray, 'articleTagsArray');
 
         /* START LOOP: for each tag */
 
+        for(let tag of articleTagsArray){
+        console.log(tag);    
+
             /* generate HTML of the link */
 
-            /* add generated code to html variable */
- 
-        /* END LOOP: for each tag */
+            const tagHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
 
+            /* add generated code to html variable */
+
+            html = html + tagHTML;
+            console.log(html, 'html');
+
+        }
+        /* END LOOP: for each tag */
         /* insert HTML of all the links into the tags wrapper */
 
+        tagsWrapper.innerHTML = html;
+    }
     /* END LOOP: for every article: */
+
+    
 }
 
 generateTags();
